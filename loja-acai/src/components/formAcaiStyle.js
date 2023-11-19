6019b55c8f6fbec5e3ae6752ec8ad152125f326c
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2023-11-18 22:14:15 -0300

    Tirando o fundo do FormAcai

--- loja-acai/src/components/formAcaiStyle.js
+++ loja-acai/src/components/formAcaiStyle.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.div`
-  background-image: url("https://e0.pxfuel.com/wallpapers/670/130/desktop-wallpaper-conheca-os-beneficios-do-acai-para-atletas-acai.jpg");
-  background-size: cover;
-  background-position: center center; 
+  
   display: flex;
   flex-direction: column;
   align-items: center;
